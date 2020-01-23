ed3c4c8734aa56db4186520253806a792a5a8563
Vladimir Eliseev <vladimir.eliseev@sibers.com>
2020-01-23 13:30:55 +0600

    initial commit, added docker config, added user management

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,8 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    MsgPhp\UserBundle\MsgPhpUserBundle::class => ['all' => true],
 ];
